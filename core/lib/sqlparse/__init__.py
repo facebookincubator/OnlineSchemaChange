f302eef77cd4469a966d58a75b50edf11c9beaf6
Luke Lu <junyilu@fb.com>
2021-06-08 11:13:33 -0700

    export Table object from model
    Summary: This is useful for typing
    
    Reviewed By: marteen
    
    Differential Revision: D28903922
    
    fbshipit-source-id: 865828b59a3ce530ae77f318b979979ec65bcff3

--- core/lib/sqlparse/__init__.py
+++ core/lib/sqlparse/__init__.py
@@ -14,7 +14,7 @@ from __future__ import unicode_literals
 
 from .create import parse_create, ParseError
 from .diff import SchemaDiff, get_type_conv_columns, need_default_ts_bootstrap
-from .models import is_equal, Column, TableIndex
+from .models import is_equal, Column, TableIndex, Table
 
 __all__ = [
     "parse_create",
@@ -25,4 +25,5 @@ __all__ = [
     "Column",
     "need_default_ts_bootstrap",
     "TableIndex",
+    "Table",
 ]
